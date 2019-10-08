ade0f2630b737f3173c9edece78cc35d9b00f014
MinJeong Kim <MinJeong.Kim@deem.com>
2019-10-08 22:35:32 +0100

    first commit

---
+++ Core/CoreService.swift
@@ -0,0 +1,15 @@
+//
+//  CoreService.swift
+//  Core
+//
+//  Created by MinJeong Kim on 08/10/2019.
+//  Copyright © 2019 MinJeong Kim. All rights reserved.
+//
+
+import Foundation
+
+class Core {
+    public static func helloWorld() -> String {
+        return "Hello Core"
+    }
+}
